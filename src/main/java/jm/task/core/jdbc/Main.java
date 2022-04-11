9974b1765c6605035ca32f8f980cc98f5afdcdd2
TmKrvc <93292083+TmKrvc@users.noreply.github.com>
2022-04-11 17:44:19 +0400

    task

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,22 +1,14 @@
 package jm.task.core.jdbc;
-
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-import jm.task.core.jdbc.util.Util;
-
-import java.sql.Array;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main {
     public static void main(String[] args) {
         UserDao userDao = new UserDaoJDBCImpl();
-
         userDao.createUsersTable();
-
         userDao.saveUser("Name1", "LastName1", (byte) 20);
         userDao.saveUser("Name2", "LastName2", (byte) 25);
         userDao.saveUser("Name3", "LastName3", (byte) 31);
@@ -28,6 +20,5 @@ public class Main {
         userDao.removeUserById(1);
         userDao.cleanUsersTable();
         userDao.dropUsersTable();
-
     }
 }
